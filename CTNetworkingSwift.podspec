914a3831f878a8a6037092f73745f3f9efcd654e
Casa Taloyum <casatwy@msn.com>
2018-10-15 23:10:36 +0800

    8

--- CTNetworkingSwift.podspec
+++ CTNetworkingSwift.podspec
@@ -16,7 +16,7 @@ Pod::Spec.new do |s|
   #
 
   s.name         = "CTNetworkingSwift"
-  s.version      = "7"
+  s.version      = "8"
   s.summary      = "CTNetworkingSwift."
   s.swift_version= "4.2"
 
--- CTNetworkingSwift/CTNetworkingSwift/Services/ServiceProtocol.swift
+++ CTNetworkingSwift/CTNetworkingSwift/Services/ServiceProtocol.swift
@@ -10,7 +10,7 @@ import Alamofire
 
 public protocol CTNetworkingService {
     var apiEnvironment : CTNetworkingAPIEnvironment { get set }
-    var sessionManager : SessionManager { get }
+    var sessionManager : SessionManager { get set }
     static var sharedInstance : CTNetworkingService { get }
     func request(params:ParamsType?, methodName:String, requestType:HTTPMethod) -> DataRequest
     func handleCommonError(_ apiManager:CTNetworkingBaseAPIManager) -> Bool
