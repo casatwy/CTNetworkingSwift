0e9f5881e4c15e4478d8b3c9d743382465143bba
Two <918308266@qq.com>
2022-01-27 15:38:50 +0800

    Update ServiceProtocol.swift
    doc:添加注释

--- CTNetworkingSwift/CTNetworkingSwift/Services/ServiceProtocol.swift
+++ CTNetworkingSwift/CTNetworkingSwift/Services/ServiceProtocol.swift
@@ -10,9 +10,13 @@ import Alamofire
 
 public protocol CTNetworkingService {
     var session : Session { get }
-    // 下面两个request协议仅需实现一个，若同时两个都实现，只有支持headers的协议生效
+    
+    // 支持headers设置的request方法，建议实现该request方法
     func request(params:Parameters?, extraURLParams:Parameters?, methodName:String, requestType:HTTPMethod, headers:HTTPHeaders?) -> URLRequest?
+    
+    // 该request方法与上面request方法只有设置headers的区别，必须实现其中一个，若同时两个都实现，只有支持headers的方法生效
     func request(params:Parameters?, extraURLParams:Parameters?, methodName:String, requestType:HTTPMethod) -> URLRequest?
+    
     func handleCommonError(_ apiManager:CTNetworkingAPIManager) -> Bool
 }
 
